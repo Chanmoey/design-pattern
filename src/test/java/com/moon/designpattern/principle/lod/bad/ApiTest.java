c3b7e84661b47cc073febc225c9e9183ead7d067
Chanmoey <relijifa@gmail.com>
2023-03-04 21:37:40 +0800

    迪米特法则。

---
+++ src/test/java/com/moon/designpattern/principle/lod/bad/ApiTest.java
@@ -0,0 +1,24 @@
+package com.moon.designpattern.principle.lod.bad;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * @author Chanmoey
+ * @date 2023年03月04日
+ */
+public class ApiTest {
+
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void test_Principal() {
+        Principal principal = new Principal();
+        Map<String, Object> map = principal.queryClazzInfo("3年1班");
+        logger.info("查询结果：{}", JSON.toJSONString(map));
+    }
+}
