28c0b0f16a79181a4f2d3c7816a59c4ec9b4a3e4
Chanmoey <relijifa@gmail.com>
2023-03-09 10:15:40 +0800

    原型模式。

---
+++ src/main/java/com/moon/designpattern/prototype/before/AnswerQuestion.java
@@ -0,0 +1,35 @@
+package com.moon.designpattern.prototype.before;
+
+/**
+ * @author Chanmoey
+ * @date 2023年03月09日
+ */
+public class AnswerQuestion {
+
+    private String name;  // 问题
+    private String key;   // 答案
+
+    public AnswerQuestion() {
+    }
+
+    public AnswerQuestion(String name, String key) {
+        this.name = name;
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
---
+++ src/test/java/com/moon/designpattern/prototype/bad/ApiTest.java
@@ -0,0 +1,19 @@
+package com.moon.designpattern.prototype.bad;
+
+import org.junit.Test;
+
+/**
+ * @author Chanmoey
+ * @date 2023年03月09日
+ */
+public class ApiTest {
+
+    @Test
+    public void test_QuestionBankController() {
+        QuestionBankController questionBankController = new QuestionBankController();
+        System.out.println(questionBankController.createPaper("花花", "1000001921032"));
+        System.out.println(questionBankController.createPaper("豆豆", "1000001921051"));
+        System.out.println(questionBankController.createPaper("大宝", "1000001921987"));
+    }
+
+}
